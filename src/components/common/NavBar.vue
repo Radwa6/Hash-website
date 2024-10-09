70be48db6eed0283253b6b82965ca92b63569426
Radwa6 <89988805+Radwa6@users.noreply.github.com>
2024-10-09 13:37:33 +0300

    commit

--- src/components/common/NavBar.vue
+++ src/components/common/NavBar.vue
@@ -198,7 +198,7 @@
               </router-link>
 
               <router-link to="/Contact">
-                <button class="">
+                <button class="button">
                   {{ $t('contact') }}
                 </button>
               </router-link>
@@ -395,6 +395,105 @@ a :hover {
   border: none;
   transition: padding 0.3s, box-shadow 0.3s, font-size 0.3s;
   color: white;
+}
+/* From Uiverse.io by zjssun */
+.button {
+  position: relative;
+  padding: 10px 42px;
+  border-radius: 6px;
+  border: none;
+  color: #fff;
+  cursor: pointer;
+  background-color: #df8317;
+  transition: all 0.2s ease;
+}
+
+.button:active {
+  transform: scale(0.96);
+}
+
+.button:before,
+.button:after {
+  position: absolute;
+  content: '';
+  width: 150%;
+  left: 50%;
+  height: 100%;
+  transform: translateX(-50%);
+  z-index: -1000;
+  background-repeat: no-repeat;
+}
+
+.button:hover:before {
+  top: -70%;
+  background-image: radial-gradient(circle, #df8317 20%, transparent 20%),
+    radial-gradient(circle, transparent 20%, #df8317 20%, transparent 30%),
+    radial-gradient(circle, #df8317 20%, transparent 20%),
+    radial-gradient(circle, #df8317 20%, transparent 20%),
+    radial-gradient(circle, transparent 10%, #df8317 15%, transparent 20%),
+    radial-gradient(circle, #df8317 20%, transparent 20%),
+    radial-gradient(circle, #df8317 20%, transparent 20%),
+    radial-gradient(circle, #df8317 20%, transparent 20%),
+    radial-gradient(circle, #df8317 20%, transparent 20%);
+  background-size: 10% 10%, 20% 20%, 15% 15%, 20% 20%, 18% 18%, 10% 10%, 15% 15%,
+    10% 10%, 18% 18%;
+  background-position: 50% 120%;
+  animation: greentopBubbles 0.6s ease;
+}
+
+@keyframes greentopBubbles {
+  0% {
+    background-position: 5% 90%, 10% 90%, 10% 90%, 15% 90%, 25% 90%, 25% 90%,
+      40% 90%, 55% 90%, 70% 90%;
+  }
+
+  50% {
+    background-position: 0% 80%, 0% 20%, 10% 40%, 20% 0%, 30% 30%, 22% 50%,
+      50% 50%, 65% 20%, 90% 30%;
+  }
+
+  100% {
+    background-position: 0% 70%, 0% 10%, 10% 30%, 20% -10%, 30% 20%, 22% 40%,
+      50% 40%, 65% 10%, 90% 20%;
+    background-size: 0% 0%, 0% 0%, 0% 0%, 0% 0%, 0% 0%, 0% 0%;
+  }
+}
+
+.button:hover::after {
+  bottom: -70%;
+  background-image: radial-gradient(circle, #df8317 20%, transparent 20%),
+    radial-gradient(circle, #df8317 20%, transparent 20%),
+    radial-gradient(circle, transparent 10%, #df8317 15%, transparent 20%),
+    radial-gradient(circle, #df8317 20%, transparent 20%),
+    radial-gradient(circle, #df8317 20%, transparent 20%),
+    radial-gradient(circle, #df8317 20%, transparent 20%),
+    radial-gradient(circle, #df8317 20%, transparent 20%);
+  background-size: 15% 15%, 20% 20%, 18% 18%, 20% 20%, 15% 15%, 20% 20%, 18% 18%;
+  background-position: 50% 0%;
+  animation: greenbottomBubbles 0.6s ease;
+}
+
+@keyframes greenbottomBubbles {
+  0% {
+    background-position: 10% -10%, 30% 10%, 55% -10%, 70% -10%, 85% -10%,
+      70% -10%, 70% 0%;
+  }
+
+  50% {
+    background-position: 0% 80%, 20% 80%, 45% 60%, 60% 100%, 75% 70%, 95% 60%,
+      105% 0%;
+  }
+
+  100% {
+    background-position: 0% 90%, 20% 90%, 45% 70%, 60% 110%, 75% 80%, 95% 70%,
+      110% 10%;
+    background-size: 0% 0%, 0% 0%, 0% 0%, 0% 0%, 0% 0%, 0% 0%;
+  }
+}
+button:hover {
+  box-shadow: inset 0px 0px 15px rgba(49, 49, 49, 0.333);
+  transform: scale(1.05);
+  transition: box-shadow 0.5s, transform 0.3s;
 }
 .menu-area li {
   display: inline-block;
--- src/components/common/ThePage.vue
+++ src/components/common/ThePage.vue
@@ -15,7 +15,7 @@
               </p>
               <div class="mt-5">
                 <router-link to="/aboutUs">
-                  <button>{{ $t('SeeMore') }}</button>
+                  <button class="button">{{ $t('SeeMore') }}</button>
                 </router-link>
               </div>
             </div>
@@ -59,7 +59,7 @@
           </h1>
           <p>{{ $t('aboutinfo') }}</p>
           <router-link to="/aboutUs">
-            <button class="mt-4">{{ $t('SeeMore') }}</button>
+            <button class="button">{{ $t('SeeMore') }}</button>
           </router-link>
           <router-link
             to="/"
@@ -463,17 +463,100 @@ export default {
   margin-top: 30px;
 }
 
-.header button,
-.about button {
-  background-color: #df8317;
-  color: #ffffff;
-  padding: 14px 15%;
-  border-radius: 10px;
+/* From Uiverse.io by zjssun */
+.button {
+  position: relative;
+  padding: 10px 42px;
+  border-radius: 6px;
   border: none;
-  transition: padding 0.3s, box-shadow 0.3s, font-size 0.3s,
-    background-color 0.5s, color 0.5s;
+  color: #fff;
+  cursor: pointer;
+  background-color: #df8317;
+  transition: all 0.2s ease;
 }
 
+.button:active {
+  transform: scale(0.96);
+}
+
+.button:before,
+.button:after {
+  position: absolute;
+  content: '';
+  width: 150%;
+  left: 50%;
+  height: 100%;
+  transform: translateX(-50%);
+  z-index: -1000;
+  background-repeat: no-repeat;
+}
+
+.button:hover:before {
+  top: -70%;
+  background-image: radial-gradient(circle, #df8317 20%, transparent 20%),
+    radial-gradient(circle, transparent 20%, #df8317 20%, transparent 30%),
+    radial-gradient(circle, #df8317 20%, transparent 20%),
+    radial-gradient(circle, #df8317 20%, transparent 20%),
+    radial-gradient(circle, transparent 10%, #df8317 15%, transparent 20%),
+    radial-gradient(circle, #df8317 20%, transparent 20%),
+    radial-gradient(circle, #df8317 20%, transparent 20%),
+    radial-gradient(circle, #df8317 20%, transparent 20%),
+    radial-gradient(circle, #df8317 20%, transparent 20%);
+  background-size: 10% 10%, 20% 20%, 15% 15%, 20% 20%, 18% 18%, 10% 10%, 15% 15%,
+    10% 10%, 18% 18%;
+  background-position: 50% 120%;
+  animation: greentopBubbles 0.6s ease;
+}
+
+@keyframes greentopBubbles {
+  0% {
+    background-position: 5% 90%, 10% 90%, 10% 90%, 15% 90%, 25% 90%, 25% 90%,
+      40% 90%, 55% 90%, 70% 90%;
+  }
+
+  50% {
+    background-position: 0% 80%, 0% 20%, 10% 40%, 20% 0%, 30% 30%, 22% 50%,
+      50% 50%, 65% 20%, 90% 30%;
+  }
+
+  100% {
+    background-position: 0% 70%, 0% 10%, 10% 30%, 20% -10%, 30% 20%, 22% 40%,
+      50% 40%, 65% 10%, 90% 20%;
+    background-size: 0% 0%, 0% 0%, 0% 0%, 0% 0%, 0% 0%, 0% 0%;
+  }
+}
+
+.button:hover::after {
+  bottom: -70%;
+  background-image: radial-gradient(circle, #df8317 20%, transparent 20%),
+    radial-gradient(circle, #df8317 20%, transparent 20%),
+    radial-gradient(circle, transparent 10%, #df8317 15%, transparent 20%),
+    radial-gradient(circle, #df8317 20%, transparent 20%),
+    radial-gradient(circle, #df8317 20%, transparent 20%),
+    radial-gradient(circle, #df8317 20%, transparent 20%),
+    radial-gradient(circle, #df8317 20%, transparent 20%);
+  background-size: 15% 15%, 20% 20%, 18% 18%, 20% 20%, 15% 15%, 20% 20%, 18% 18%;
+  background-position: 50% 0%;
+  animation: greenbottomBubbles 0.6s ease;
+}
+
+@keyframes greenbottomBubbles {
+  0% {
+    background-position: 10% -10%, 30% 10%, 55% -10%, 70% -10%, 85% -10%,
+      70% -10%, 70% 0%;
+  }
+
+  50% {
+    background-position: 0% 80%, 20% 80%, 45% 60%, 60% 100%, 75% 70%, 95% 60%,
+      105% 0%;
+  }
+
+  100% {
+    background-position: 0% 90%, 20% 90%, 45% 70%, 60% 110%, 75% 80%, 95% 70%,
+      110% 10%;
+    background-size: 0% 0%, 0% 0%, 0% 0%, 0% 0%, 0% 0%, 0% 0%;
+  }
+}
 button:hover {
   box-shadow: inset 0px 0px 15px rgba(49, 49, 49, 0.717);
   transform: scale(1.05);
